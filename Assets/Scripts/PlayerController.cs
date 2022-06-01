468f7dab68b3770faa75aef0427d52334ab07b71
Cristóbal Troncoso <cristobaltroncoso@gmail.com>
2022-05-31 23:38:10 -0400

    Adds jump feature to player controller. Character jumps using spacebar.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,13 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float playerSpeed;
+    [SerializeField] private float jumpSpeed;
     private PlayerInput _playerInput;
     private CharacterController _characterController;
     private Vector2 _currentMovementInput;
     private Vector3 _currentMovement;
-    private bool _isMovementPressed;
+    private float _ySpeed;
+    private bool _isJumpPressed;
 
     private void Awake()
     {
@@ -21,21 +23,40 @@ public class PlayerController : MonoBehaviour
         _playerInput.CharacterControls.Move.started += OnMovementInput;
         _playerInput.CharacterControls.Move.performed += OnMovementInput;
         _playerInput.CharacterControls.Move.canceled += OnMovementInput;
-    }
 
-    // Update is called once per frame
+        _playerInput.CharacterControls.Jump.started += OnJump;
+        _playerInput.CharacterControls.Jump.canceled += OnJump;
+    }
+    
     void Update()
     {
-        _characterController.Move(_currentMovement * Time.deltaTime * playerSpeed);
+        _ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        if (_characterController.isGrounded)
+        {
+            _ySpeed = -0.5f;
+            
+            if (_isJumpPressed)
+            {
+                _ySpeed = jumpSpeed;
+            }
+        }
+
+        Vector3 currentMovement = _currentMovement * playerSpeed;
+        currentMovement.y = _ySpeed;
+        _characterController.Move(currentMovement * Time.deltaTime);
     }
 
     private void OnMovementInput(InputAction.CallbackContext context)
     {
-        Debug.Log(context.ReadValue<Vector2>());
         _currentMovementInput = context.ReadValue<Vector2>();
         _currentMovement.x = _currentMovementInput.x;
         _currentMovement.z = _currentMovementInput.y;
-        _isMovementPressed = (_currentMovementInput.x != 0 || _currentMovementInput.y != 0);
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        _isJumpPressed = context.ReadValueAsButton();
     }
 
     private void OnEnable()

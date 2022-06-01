a3ee70b8f47b23cb5baf35d3d59349b0fc2d96ec
Cristóbal Troncoso <cristobaltroncoso@gmail.com>
2022-05-31 22:41:02 -0400

    Adds main player object to scene that uses Unity's input system and character controller to move the player character using WASD keys

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float playerSpeed;
+    private PlayerInput _playerInput;
+    private CharacterController _characterController;
+    private Vector2 _currentMovementInput;
+    private Vector3 _currentMovement;
+    private bool _isMovementPressed;
+
+    private void Awake()
     {
+        _playerInput = new PlayerInput();
+        _characterController = GetComponent<CharacterController>();
         
+        _playerInput.CharacterControls.Move.started += OnMovementInput;
+        _playerInput.CharacterControls.Move.performed += OnMovementInput;
+        _playerInput.CharacterControls.Move.canceled += OnMovementInput;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _characterController.Move(_currentMovement * Time.deltaTime * playerSpeed);
+    }
+
+    private void OnMovementInput(InputAction.CallbackContext context)
+    {
+        Debug.Log(context.ReadValue<Vector2>());
+        _currentMovementInput = context.ReadValue<Vector2>();
+        _currentMovement.x = _currentMovementInput.x;
+        _currentMovement.z = _currentMovementInput.y;
+        _isMovementPressed = (_currentMovementInput.x != 0 || _currentMovementInput.y != 0);
+    }
+
+    private void OnEnable()
+    {
+        _playerInput.CharacterControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerInput.CharacterControls.Disable();
     }
 }
